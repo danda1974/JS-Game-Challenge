6a1ff19f3cd17cf9e31acbca55385cac6e8de0ba
Daniela Spacek <familie.spacek@googlemail.com>
2022-04-27 18:03:08 +0200

    draft js with cardArray

---
+++ index.js
@@ -0,0 +1,34 @@
+const cardArray = [
+  {
+    name: "HK1",
+    img: "images/HelloKitty1.png",
+  },
+  {
+    name: "HK2",
+    img: "images/HelloKitty2.png",
+  },
+  {
+    name: "HK3",
+    img: "images/HelloKitty3.png",
+  },
+  {
+    name: "HK4",
+    img: "images/HelloKitty4.png",
+  },
+  {
+    name: "HK5",
+    img: "images/HelloKitty5.png",
+  },
+  {
+    name: "HK6",
+    img: "images/HelloKitty6.png",
+  },
+  {
+    name: "HK7",
+    img: "images/HelloKitty7.png",
+  },
+  {
+    name: "HK8",
+    img: "images/HelloKitty8.png",
+  },
+];
