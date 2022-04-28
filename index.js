066c505aa9cb71ea8e76f2e3c7d2b054c0a1041b
Daniela Spacek <familie.spacek@googlemail.com>
2022-04-28 15:17:54 +0200

    more detailed ReadMe and changed colors in css

--- index.js
+++ index.js
@@ -1,34 +1,69 @@
-const cardArray = [
-  {
-    name: "HK1",
-    img: "images/HelloKitty1.png",
-  },
-  {
-    name: "HK2",
-    img: "images/HelloKitty2.png",
-  },
-  {
-    name: "HK3",
-    img: "images/HelloKitty3.png",
-  },
-  {
-    name: "HK4",
-    img: "images/HelloKitty4.png",
-  },
-  {
-    name: "HK5",
-    img: "images/HelloKitty5.png",
-  },
-  {
-    name: "HK6",
-    img: "images/HelloKitty6.png",
-  },
-  {
-    name: "HK7",
-    img: "images/HelloKitty7.png",
-  },
-  {
-    name: "HK8",
-    img: "images/HelloKitty8.png",
-  },
-];
+//card options
+document.addEventListener("DOMContentLoaded", () => {
+  const cardArray = [
+    {
+      name: "HK1",
+      img: "images/HelloKitty1.png",
+    },
+    {
+      name: "HK2",
+      img: "images/HelloKitty2.png",
+    },
+    {
+      name: "HK3",
+      img: "images/HelloKitty3.png",
+    },
+    {
+      name: "HK4",
+      img: "images/HelloKitty4.png",
+    },
+    {
+      name: "HK5",
+      img: "images/HelloKitty5.png",
+    },
+    {
+      name: "HK6",
+      img: "images/HelloKitty6.png",
+    },
+    {
+      name: "HK7",
+      img: "images/HelloKitty7.png",
+    },
+    {
+      name: "HK8",
+      img: "images/HelloKitty8.png",
+    },
+    {
+      name: "HK1",
+      img: "images/HelloKitty1.png",
+    },
+    {
+      name: "HK2",
+      img: "images/HelloKitty2.png",
+    },
+    {
+      name: "HK3",
+      img: "images/HelloKitty3.png",
+    },
+    {
+      name: "HK4",
+      img: "images/HelloKitty4.png",
+    },
+    {
+      name: "HK5",
+      img: "images/HelloKitty5.png",
+    },
+    {
+      name: "HK6",
+      img: "images/HelloKitty6.png",
+    },
+    {
+      name: "HK7",
+      img: "images/HelloKitty7.png",
+    },
+    {
+      name: "HK8",
+      img: "images/HelloKitty8.png",
+    },
+  ];
+});
